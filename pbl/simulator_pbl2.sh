995849c7b1a7c6d2cb31c4826ea6341d6f46efbe
Takeshi Ohkawa <ohkawa.takeshi@tokai.ac.jp>
2021-08-16 11:42:49 +0900

    Fixed PLUGIN path

--- pbl/simulator_pbl2.sh
+++ pbl/simulator_pbl2.sh
@@ -2,6 +2,7 @@
 
 export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/home/ubuntu/gazebo_models_worlds_collection/models
 export GAZEBO_RESOURCE_PATH=$GAZEBO_RESOURCE_PATH:/home/ubuntu/gazebo_models_worlds_collection/worlds
+export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:/opt/ros/melodic/lib/
 
 source /home/ubuntu/catkin_ws/devel/setup.bash
 rosrun gazebo_ros gazebo --verbose ~/pbl2.world
