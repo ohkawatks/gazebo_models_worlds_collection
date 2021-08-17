9d6c6b92f44dd502c5035f3af47efd77f1cab91c
Takeshi Ohkawa <ohkawa.takeshi@tokai.ac.jp>
2021-08-17 03:38:45 +0000

    Fixed bugs on models and scripts

--- pbl/simulator_pbl.sh
+++ pbl/simulator_pbl.sh
@@ -3,6 +3,11 @@
 export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/home/ubuntu/gazebo_models_worlds_collection/models
 export GAZEBO_RESOURCE_PATH=$GAZEBO_RESOURCE_PATH:/home/ubuntu/gazebo_models_worlds_collection/worlds
 export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:/opt/ros/melodic/lib/
+#export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:/home/ubuntu/catkin_ws/devel/lib/
+echo $GAZEBO_PLUGIN_PATH
 
 source /home/ubuntu/catkin_ws/devel/setup.bash
+
 rosrun gazebo_ros gazebo --verbose worlds/iris_arducopter_yosemite.world
+#rosrun gazebo_ros debug --verbose worlds/iris_arducopter_yosemite.world
+
